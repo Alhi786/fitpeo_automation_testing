435d197ecaf9ff003339121822ecc5cd9870b918
Alhijas Salim <alhijassalim8@gmail.com>
2024-11-27 03:38:11 +0530

    initial commit
    Signed-off-by: Alhijas Salim <alhijassalim8@gmail.com>

---
+++ src/org/fitpeo/automation/FitpeoUIAutomation.java
@@ -0,0 +1,80 @@
+package org.fitpeo.automation;
+
+import org.fitpeo.automation.common.FitpeoAutomationConst;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class FitpeoUIAutomation {
+
+	private static WebDriver driver;
+	private static Actions action;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		initialize();
+		WebElement value = driver.findElement(By.xpath(FitpeoAutomationConst.REVENUE_CALCULATOR_PAGE_X_PATH));
+		value.click();
+		Thread.sleep(2000);
+		WebElement slider = driver.findElement(By.xpath(FitpeoAutomationConst.SLIDER_X_PATH));
+		Thread.sleep(2000);
+		int desiredValue = 820;
+		WebElement patientsInputElement = driver.findElement(By.id(FitpeoAutomationConst.SLIDER_INPUT_ID));
+		int currentValue = 0;
+		boolean isCompleted = false;
+		int offset = 10;
+		while (!isCompleted) {
+			currentValue = Integer.parseInt(patientsInputElement.getAttribute("value"));
+			if (desiredValue - currentValue < 100) {
+				offset = 2;
+			}
+			if (currentValue <= desiredValue) {
+				action.dragAndDropBy(slider, offset, 0).perform();
+			} else {
+				action.dragAndDropBy(slider, -1, 0).perform();
+				isCompleted = true;
+				break;
+			}
+		}
+		action.release(slider);
+		Thread.sleep(2000);
+		action.scrollToElement(patientsInputElement).perform();
+		patientsInputElement.sendKeys(Keys.chord(Keys.CONTROL, "a"), "560");
+		Thread.sleep(2000);
+		patientsInputElement.sendKeys(Keys.chord(Keys.CONTROL, "a"), "820");
+		Thread.sleep(1000);
+		tickCheckBoxByXPath(FitpeoAutomationConst.CHECKBOX_ONE_X_PATH);
+		Thread.sleep(1000);
+		tickCheckBoxByXPath(FitpeoAutomationConst.CHECKBOX_TWO_X_PATH);
+		Thread.sleep(1000);
+		tickCheckBoxByXPath(FitpeoAutomationConst.CHECKBOX_THREE_X_PATH);
+		Thread.sleep(1000);
+		tickCheckBoxByXPath(FitpeoAutomationConst.CHECKBOX_FOUR_X_PATH);
+
+	}
+	
+	private static void initialize() {
+		driver =  new ChromeDriver();
+		action = new Actions(driver);
+		driver.get("https://fitpeo.com/");
+		driver.manage().window().maximize();
+	}
+	
+	private static void tickCheckBoxByXPath(String xPath) throws InterruptedException {
+		try {
+			WebElement checkbox = driver
+					.findElement(By.xpath(xPath));
+			action.scrollToElement(checkbox).perform();
+			checkbox.click();
+		} catch (NoSuchElementException e) {
+			System.out.println("Re check for element");
+			Thread.sleep(1000);
+			tickCheckBoxByXPath(xPath);
+		}
+	}
+
+}
---
+++ src/org/fitpeo/automation/common/FitpeoAutomationConst.java
@@ -0,0 +1,13 @@
+package org.fitpeo.automation.common;
+
+public class FitpeoAutomationConst {
+
+	public static final String REVENUE_CALCULATOR_PAGE_X_PATH = "/html/body/div[1]/div/header/div/div[3]/div[6]/a/div";
+	public static final String SLIDER_X_PATH = "//input[@type='range']";
+	public static final String SLIDER_INPUT_ID = ":r0:";
+
+	public static final String CHECKBOX_ONE_X_PATH = "/html/body/div[2]/div[1]/div[2]/div[1]/label/span[1]/input";
+	public static final String CHECKBOX_TWO_X_PATH = "/html/body/div[2]/div[1]/div[2]/div[2]/label/span[1]/input";
+	public static final String CHECKBOX_THREE_X_PATH = "/html/body/div[2]/div[1]/div[2]/div[3]/label/span[1]/input";
+	public static final String CHECKBOX_FOUR_X_PATH = "/html/body/div[2]/div[1]/div[2]/div[8]/label/span[1]/input";
+}
